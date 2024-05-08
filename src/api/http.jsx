770aec82fa29005a9b3d6032c42f1288509af00d
Nurlana Quluyeva <81523883+nurlana-qlyva@users.noreply.github.com>
2024-05-08 14:45:29 +0300

    set

--- src/api/http.jsx
+++ src/api/http.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const token = JSON.parse(localStorage.getItem("token"))
+
 const http = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_URL,
   headers: {
     "Content-type": "application/json",
-    Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))?.value}`,
+    Authorization: `Bearer ${token.value}`,
   },
 });
 
